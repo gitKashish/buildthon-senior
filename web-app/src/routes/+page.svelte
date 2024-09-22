5796df376d28a38732d884fb67604d5a50599e2c
Kashish Sahu <125540624+gitKashish@users.noreply.github.com>
2024-09-22 20:34:57 +0530

    Added tailwindcss, and created ui for homepage

--- web-app/src/routes/+page.svelte
+++ web-app/src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<div class="flex flex-col gap-4 justify-center items-center min-h-screen p-6">
+    <div class="flex flex-col justify-center items-center text-slate-600 text-6xl min-h-20 font-extrabold">
+        DeVote
+        <div class="flex justify-center items-center text-slate-500 text-xl">
+            Decentralized Voting
+            </div>
+        </div>
+    <div class="w-full max-w-sm space-y-4">
+        <input 
+            class="rounded-lg border border-gray-300 p-3 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500" 
+            type="text" 
+            placeholder="Enter the Poll ID"
+        />
+        <button 
+            class="rounded-lg bg-indigo-500 text-white font-medium w-full h-12 hover:bg-indigo-600 transition duration-300 ease-in-out"
+        >
+            Submit
+        </button>
+        <button 
+            class="rounded-lg border border-indigo-500 text-indigo-500 font-medium w-full h-12 hover:bg-indigo-500 hover:text-white transition duration-300 ease-in-out"
+        >
+            Create Poll
+        </button>
+    </div>
+</div>
---
+++ web-app/src/routes/[pollId]/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import {page} from '$app/stores';
+    const pollId = $page.params.pollId;
+</script>
+
+<h1>{pollId}</h1>
